627d1315b194aac22a1edb7ef2205dcab5e0dea1
tejareddymigari <tejareddymigari@gmail.com>
2023-02-22 15:56:28 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+
+
+class CbvContext(TemplateView):
+    template_name='CbvContext.html'
+
+
+
+    
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['name']='tejareddy'
+        context['age']=23
+        return context
